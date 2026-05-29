990699b3956b550b1c47fdea281a5c753a627b18
Melianiski <ruarteandres96@gmail.com>
2026-05-29 06:06:43 -0300

    Practica 16

---
+++ practica-16/js/script.js
@@ -0,0 +1,22 @@
+let nombre = "Andres";
+let apellido = "Ruarte";
+
+console.log(nombre);
+console.log(apellido);
+
+
+
+
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+
+
+
+let edad = 29;
+console.log(edad);
+
+
+
+const universidad = "UNA Artes multimediales";
+console.log(universidad);
