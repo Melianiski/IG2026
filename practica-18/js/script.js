7a50515ede89a3457a3b4530d52aa25b24c142a2
Melianiski <ruarteandres96@gmail.com>
2026-05-30 03:22:01 -0300

    Practica 18

---
+++ practica-18/js/script.js
@@ -0,0 +1,21 @@
+alert ("Al aceptar cambia todo");
+
+let titulo = document.querySelector("h1");
+titulo.style.color = "red";
+titulo.style.fontSize = "5em";
+
+titulo.innerText = titulo.innerText += " - Fin del mundo";
+
+
+let parrafos = document.querySelector("p");
+parrafos.style.color = "blue";
+parrafos.innerText = parrafos.innerText += " - Fin del mundo";
+
+
+let fuerte = document.querySelector("strong");
+fuerte.style.color = "green";
+fuerte.innerText = fuerte.innerText += ".... o moris";
+
+
+console.log("De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir. De que sirve vivir, si vamos a morir.");
+console.log("Tan simple como eso")
